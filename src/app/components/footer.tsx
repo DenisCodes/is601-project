0ed469b6d10846325a9d391a1f998a88c232ebeb
deniscodes <smirnovadenis@gmail.com>
2023-12-19 18:09:43 -0500

    removed comments

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,14 +4,12 @@ const Footer = () => {
   return (
     <footer className="text-black py-8 border-t border-yellow-500">
       <div className="container mx-auto flex justify-between">
-        {/* Location */}
         <div className="w-1/3 pr-4 border-r border-yellow-500 flex flex-col items-center">
           <h3 className="text-lg font-semibold mb-4">Location</h3>
           <p>130 Fulton St, New York</p>
           <p>NY 10038, USA</p>
         </div>
 
-        {/* Hours */}
         <div className="w-1/3 px-4 border-r border-yellow-500 flex flex-col items-center">
           <h3 className="text-lg font-semibold mb-4">Hours</h3>
           <p>Mon - Sat: 11:00 am - 9:00 pm</p>
@@ -19,7 +17,6 @@ const Footer = () => {
           <p>Dine-in and Take-out!</p>
         </div>
 
-        {/* Socials */}
         <div className="w-1/3 pl-4 flex flex-col items-center">
           <h3 className="text-lg font-semibold mb-4">Socials</h3>
           <p>Instagram</p>
