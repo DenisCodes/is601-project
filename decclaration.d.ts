569a68066934fd176065a7002ceec7dc2fa4af83
deniscodes <smirnovadenis@gmail.com>
2023-12-19 20:38:38 -0500

    I hate configs

---
+++ decclaration.d.ts
@@ -0,0 +1,8 @@
+declare module '*.png';
+declare module '*.PNG';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+declare module '*.bmp';
+declare module '*.tiff';
