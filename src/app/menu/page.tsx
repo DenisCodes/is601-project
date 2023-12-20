599e9bb67c19ca7dbdff70fae0d18c21d20ea0b0
deniscodes <smirnovadenis@gmail.com>
2023-12-19 22:10:39 -0500

    added menu section

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,20 +2,14 @@
 import {NextUIProvider} from "@nextui-org/react";
 import Navybar from '../components/navbar';
 import Footer from '../components/footer';
-
-function MenuNav(){
-  return (
-    <div>
-        menu
-    </div>
-  )
-}
+import Menus from '../components/menus';
 
 export default function Menu() {
   return (
     <NextUIProvider>
         <Navybar />
-        <MenuNav/>
+        <h1 className="flex justify-center items-center">Menu</h1>
+        <Menus/>
         <Footer />
     </NextUIProvider>
   )
