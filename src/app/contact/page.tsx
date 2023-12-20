4fed3e926762ecd691cdfb49a74c8ff661e4748e
deniscodes <smirnovadenis@gmail.com>
2023-12-19 22:13:48 -0500

    adjusted image size

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import Navybar from '../components/navbar';
 import NewsLetter from "../components/newsletter";
 import Footer from '../components/footer';
 import Image from 'next/image'
-import Chef from '../images/Frame27.PNG';
+import Chef from '../images/Frame27.png';
 
 export default function Contact() {
   return (
@@ -17,7 +17,7 @@ export default function Contact() {
         </div>
 
         <div>
-          <Image src={Chef} alt="Chef" width={250} height={250} />
+          <Image src={Chef} alt="Chef" width={375} height={375} />
           <br></br>
           <div>If you have any questions about your order please call:</div>
           <br></br>
